71bd8cfd5468c5d243cb1f22f6a7acbe57559b54
Akihiro Sada <sada@netlab.jp>
2015-02-07 18:18:04 +0900

    Reservationモデルの関連付けを追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,9 @@ class Reservation < ActiveRecord::Base
     APPROVED = 2, # 承認
     DENIED   = 3, # 拒否
   ]
+
+  belongs_to :user
+  belongs_to :parking
+
+  validates :start_at, :end_at, :status, presence: true
 end
