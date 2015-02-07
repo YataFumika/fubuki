e77b4909a03a9cf656a13a779b361821e4657867
shinoharat <ashikishukyu@gmail.com>
2015-02-07 19:29:58 +0900

    定数を追加

--- app/models/allow_time.rb
+++ app/models/allow_time.rb
@@ -1,4 +1,14 @@
 class AllowTime < ActiveRecord::Base
+  WEEKDAY_LIST = [
+    MON = { code: 1, label: "月" },
+    TUE = { code: 2, label: "火" },
+    WED = { code: 3, label: "水" },
+    THU = { code: 4, label: "木" },
+    FRI = { code: 5, label: "金" },
+    SAT = { code: 6, label: "土" },
+    SUN = { code: 7, label: "日" },
+  ]
+
   has_many :parkings
 
   validates :weekday, :start_time, :end_time, presence: true
