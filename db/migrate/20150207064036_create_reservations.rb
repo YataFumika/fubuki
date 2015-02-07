f25f1a5f43778ebffaebed155ef157d1710b07df
matsue-city05 <matsue-city05@matsue-city05-no-MacBook-Pro.local>
2015-02-07 15:47:24 +0900

    reservations

---
+++ db/migrate/20150207064036_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.integer :user_id
+      t.integer :parking_id
+      t.datetime :start_at
+      t.datetime :end_at
+      t.intger :status
+
+      t.timestamps
+    end
+  end
+end
