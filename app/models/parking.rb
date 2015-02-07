f35748c5fd61851cd3c77b1f51e692351a3a2d89
Akihiro Sada <sada@netlab.jp>
2015-02-07 18:16:59 +0900

    Parkingモデルの関連付けを追加

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,3 +1,9 @@
 class Parking < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
+
+  has_many :allow_times
+  has_many :deny_dates
+  belongs_to :user
+
+  validates :name, :memo, presence: true
 end
