889dcd2c744c9d1e68e95c6c9eb8775074722d06
Akihiro Sada <sada@netlab.jp>
2015-02-07 15:58:25 +0900

    parkingsのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Fubuki::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :users
+  resources :parkings
 
   # Example resource route with options:
   #   resources :products do
