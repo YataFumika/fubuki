875880dafc906d047d629b7f4ab73a6ac8bdd5c6
YataFumika <vulpes1793@gmai.com>
2015-02-07 15:17:46 +0900

    Rails Project new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fubuki::Application.config.secret_key_base = '2c5fc902448d2d3875d45f26f0cc985be678d9d08fa9fbcce0242a3288e1889870d1eb96027427138f5d168646bbac7d36a783b47534ddf3f3084717050defe1'
