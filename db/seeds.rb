00f55a5278e5544f2d09f3daaa05c062568d6f37
matsue-city05 <matsue-city05@matsue-city05-no-MacBook-Pro.local>
2015-02-08 13:28:38 +0900

    seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,24 +12,127 @@ u2 = User.create(name: "test2", point: 100, email: "test2@test.com", password: "
 u3 = User.create(name: "test3", point: 100, email: "test3@test.com", password: "test", password_confirmation: "test")
 
 Parking.destroy_all
-Parking.create(
+p1 = Parking.create(
   address: "島根県松江市123-1",
   name: "駐車場１",
   user_id: u1.id,
   memo: "101が該当の駐車場です。",
 )
 
-Parking.create(
+p2 = Parking.create(
   address: "島根県松江市456-1",
   name: "駐車場２",
   user_id: u2.id,
   memo: "よろしくお願いします。",
 )
 
-Parking.create(
+p3 = Parking.create(
   address: "島根県松江市789-1",
   name: "駐車場３",
   user_id: u3.id,
   memo: "よろしくお願いします。",
 )
+
+AllowTime.destroy_all
+AllowTime.create(
+  parking_id: p1.id,
+  weekday: 1,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p1.id,
+  weekday: 2,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p1.id,
+  weekday: 3,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p1.id,
+  weekday: 4,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p1.id,
+  weekday: 5,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p1.id,
+  weekday: 6,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p1.id,
+  weekday: 7,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p2.id,
+  weekday: 1,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p2.id,
+  weekday: 3,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p2.id,
+  weekday: 5,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p2.id,
+  weekday: 7,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+AllowTime.create(
+  parking_id: p3.id,
+  weekday: 1,
+  start_time: "1000",
+  end_time: "1800"
+)
+
+DenyDate.destroy_all
+
+DenyDate.create(
+  parking_id: p1.id,
+  date: Date.new(2015, 2 , 9)
+)
+
+DenyDate.create(
+  parking_id: p1.id,
+  date: Date.new(2015, 2 , 10)
+)
+
+DenyDate.create(
+  parking_id: p2.id,
+  date: Date.new(2015, 2 , 9)
+)
+
 
