9983007db9e46dad870c5affa431dd66297cf611
matsue-city05 <matsue-city05@matsue-city05-no-MacBook-Pro.local>
2015-02-08 11:27:48 +0900

    メインメニューの導入

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,7 +6,8 @@ class LoginsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_name params[:name]
+    @name = params[:name]
+    @user = User.find_by_name @name
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash.now.alert = "ログインに成功しました"
