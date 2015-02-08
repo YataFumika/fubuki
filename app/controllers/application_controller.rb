b35a808110f7132770953b0a2286995b70a40ed7
YataFumika <vulpes1793@gmai.com>
2015-02-08 09:41:33 +0900

    ユーザ認証機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_action :login_check
+  before_action :login_check
+
+private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-private
-
   def login_check
-    if (session[:user_id].blank?)
+    unless current_user
       redirect_to root_path
     end
   end
